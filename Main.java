eaad9a75f583381ffe085204aed56a9e6f8267c0
Blokoy <j97873661@gmail.com>
2026-04-07 23:35:44 +0800

    first commit

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // SwingUtilities ensures the UI is built on the correct thread
+        SwingUtilities.invokeLater(() -> {
+            TrackerUI appWindow = new TrackerUI();
+            appWindow.setVisible(true);
+        });
     }
 }
